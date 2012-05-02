5a731ce1b3486d68c189a721f6a68861aa8f765b
Bruno Coimbra <bbcoimbra@gmail.com>
2012-05-01 22:59:04 -0300

    try to load shared objects from many places
    I didn't understood why spec Raketask try to load rb_getifaddrs.so
    from lib/. When gem get installed the shared object was placed to
    lib/system/.

--- lib/system/getifaddrs.rb
+++ lib/system/getifaddrs.rb
@@ -1,1 +1,5 @@
-require File.join(File.dirname(__FILE__), 'rb_getifaddrs')
+begin
+  require File.join(File.dirname(__FILE__), 'rb_getifaddrs')
+rescue LoadError
+  require File.join(File.dirname(__FILE__), '..', 'rb_getifaddrs')
+end
