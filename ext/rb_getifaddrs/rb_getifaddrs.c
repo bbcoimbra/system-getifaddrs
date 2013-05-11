c704f7aac6db2249c14a5c1dc6bf5f32999f8fa6
Conrad Irwin <conrad.irwin@gmail.com>
2013-05-10 20:33:01 -0700

    Add support for getting all ifaddrs
    We have some systems that have multiple IP addresses on some interfaces,
    so I've added a second function System.get_all_ifaddrs to retreive them
    all.

--- ext/rb_getifaddrs/rb_getifaddrs.c
+++ ext/rb_getifaddrs/rb_getifaddrs.c
@@ -34,35 +34,41 @@ int get_if_netmask(struct ifaddrs *ifa, char *netmask){
 	return 1;
 }
 
-VALUE set_if_hash(VALUE rb_if_hash, char *if_name, char *if_host, char *if_netmask){
+VALUE if_hash(struct ifaddrs *ifa)
+{
+    VALUE hash = rb_hash_new();
 
-	VALUE rb_if_data_hash;
-	char *str_inet_name = "inet_addr";
-	char *str_netmask_name = "netmask";
+    char *if_host, *if_netmask, *if_name;
 
-	rb_if_data_hash = rb_hash_new();
-	rb_hash_aset(rb_if_hash,
-			rb_str_intern(rb_str_new2(if_name)),
-			rb_if_data_hash);
-	rb_hash_aset(rb_if_data_hash,
-			rb_str_intern(rb_str_new2(str_inet_name)),
-			rb_str_new2(if_host));
-	rb_hash_aset(rb_if_data_hash,
-			rb_str_intern(rb_str_new2(str_netmask_name)),
-			rb_str_new2(if_netmask));
-	return rb_if_data_hash;
+    if_name = get_if_name(ifa);
+    if_host = malloc(sizeof(char) * NI_MAXHOST);
+    if_netmask = malloc(sizeof(char) * NI_MAXHOST);
+
+    rb_hash_aset(hash, ID2SYM(rb_intern("interface")), rb_str_new2(if_name));
+
+    if (get_if_host(ifa, if_host))
+       rb_hash_aset(hash, ID2SYM(rb_intern("inet_addr")), rb_str_new2(if_host));
+    else
+       rb_raise(rb_eSystemCallError, "Can't get IP from %s", if_name);
+
+    if (get_if_netmask(ifa, if_netmask))
+       rb_hash_aset(hash, ID2SYM(rb_intern("netmask")), rb_str_new2(if_netmask));
+    else
+       rb_raise(rb_eSystemCallError, "Can't get netmask from %s", if_name);
+
+    return hash;
 }
 
-VALUE rb_get_ifaddrs(void)
+VALUE rb_get_all_ifaddrs(void)
 {
     struct ifaddrs *ifaddr, *ifa;
-    VALUE rb_if_hash;
+    VALUE rb_if_arr;
 
     if (getifaddrs(&ifaddr) == -1)
     {
         rb_raise(rb_eSystemCallError, "Can't get info about interfaces");
     }
-    rb_if_hash = rb_hash_new();
+    rb_if_arr = rb_ary_new();
     for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
     {
         int family;
@@ -70,27 +76,17 @@ VALUE rb_get_ifaddrs(void)
         family = get_if_family(ifa);
         if (family == AF_INET)
         {
-            char *if_host, *if_netmask, *if_name;
-
-            if_name = get_if_name(ifa);
-						if_host = malloc(sizeof(char) * NI_MAXHOST);
-						if (! get_if_host(ifa, if_host))
-							rb_raise(rb_eSystemCallError, "Can't get IP from %s", if_name);
-
-						if_netmask = malloc(sizeof(char) * NI_MAXHOST);
-						if (! get_if_netmask(ifa, if_netmask))
-							rb_raise(rb_eSystemCallError, "Can't get IP from %s", if_name);
-
-						set_if_hash(rb_if_hash, if_name, if_host, if_netmask);
+            rb_ary_push(rb_if_arr, if_hash(ifa));
         }
     }
+
     freeifaddrs(ifaddr);
-    return rb_if_hash;
+    return rb_if_arr;
 }
 
 VALUE mSystem;
 void Init_rb_getifaddrs(){
     mSystem = rb_define_module("System");
-    rb_define_module_function(mSystem, "get_ifaddrs", rb_get_ifaddrs, 0);
+    rb_define_module_function(mSystem, "get_all_ifaddrs", rb_get_all_ifaddrs, 0);
 }
 
--- lib/system/getifaddrs.rb
+++ lib/system/getifaddrs.rb
@@ -3,3 +3,11 @@ begin
 rescue LoadError
   require File.join(File.dirname(__FILE__), '..', 'rb_getifaddrs')
 end
+
+module System
+  def self.get_ifaddrs
+    get_all_ifaddrs.each_with_object({}) do |data, hash|
+      hash[data[:interface].to_sym] = {inet_addr: data[:inet_addr], netmask: data[:netmask]}
+    end
+  end
+end
